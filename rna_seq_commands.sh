e98bc212edbfed179ca121532e48d32bb407a6ae
Guannan Shen <guannan.shen.ai@gmail.com>
2018-07-19 17:17:09 -0600

    Add fastqc, index human hg38 and .gtf

--- rna_seq_commands.sh
+++ rna_seq_commands.sh
@@ -3,6 +3,7 @@
 #!/bin/sh
 more [file_name]  # to view file
 more ~/.bashrc    ## the startup file 
+less [file_name]  # to view large multi lines file 
 
 ## text editor 
 
@@ -22,7 +23,7 @@ gunzip -c [filename] | head           ## to quick review the head of compressed 
 
 ## set path to python 3.5
 
-## view all current running process
+## view all computing power usage
 htop
 
 ## for the test proj, trim
@@ -42,6 +43,82 @@ cd trimmedReads
 fastqc -t 3 *.fastq.gz
 
 # run the summariseFastQC.py
+python3 /home/sheng/my1stproj/bucket/code/summarizeFastQC.py 
+
+## Redo
+echo --------------------------------------------------------------------------------------------
+echo redo everything 'for' the whole dataset
+echo 'commands only for' remmote server, with proper system, software installed and data 'file mounted'
+echo --------------------------------------------------------------------------------------------
+
+## quick check data
+## one file at a time
+gunzip -c [filename] | head #the -c option write on standard output, keep original files unchanged
+
+## get counts summary trim summary with .py and cutadapt
+## check python3 --version
+python3 --version
+
+## data is in rawReads
+python3 /home/sheng/my1stproj/bucket/code/trimBatch.py -c --cutadapt-q 20 --cutadapt-m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -U -o /home/sheng/my1stproj/bucket/trimmedReads -i .fastq.gz /home/sheng/my1stproj/bucket/rawReads
+
+## or using ~ instead of home
+## download cutadapt 1.16 version with pip
+python3 ~/my1stproj/bucket/code/trimBatch.py -c --cutadapt-q 20 --cutadapt-m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -U -o ~/my1stproj/bucket/trimmedReads -i .fastq.gz ~/my1stproj/bucket/rawReads
+
+##
+echo ---------------------------------------------------
+echo meanwhile, connect the server 'in' another terminal with 'cd' and 'ssh'
+echo using 'htop' to monitor the server usage
+echo '14' files take up to around '50 min'
+echo ---------------------------------------------------
+
+## after this get the FastQC plot to check the quality of the data 
+## fastqc is required to be downloaded
+cd ~/my1stproj/bucket/trimmedReads
+fastqc -t 20 *.fastq.gz                    # -t option for thread, for this 100gb ram, 32cpu server 20 is fine 
+
+#Make fastqc summary excel spread sheet. This will output a .csv called all_mod_scores.csv. 
+#It is coded as 1 = PASS, 0 = WARNING and -1 = FAIL. You will need to add the column titles.
+python3 ~/my1stproj/bucket/code/summarizeFastQC.py
+
+## Prepare index files 
+cd ~/my1stproj/bucket/index
+mkdir hum_index
+cd hum_index
+
+## decompress genome file, the official human genome sequence
+## UCSC Genome Browser (http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/)
+## hg38.fa.gz
+gunzip hg38.fa.gz  # this will delete the input file, -k is keep 
+
+
+## insert blank line
+sed '/>/{x;p;x}' ~/my1stproj/bucket/index/hum_index/hg38.fa > hg38.tmp.fa
+
+#remove contig sequences
+sed -i '/KI/,/^$/d' ~/my1stproj/bucket/index/hum_index/hg38.tmp.fa
+sed -i '/GL/,/^$/d' ~/my1stproj/bucket/index/hum_index/hg38.tmp.fa
+sed -i '/alt/,/^$/d' ~/my1stproj/bucket/index/hum_index/hg38.tmp.fa
+sed '/^$/d' ~/my1stproj/bucket/index/hum_index/hg38.tmp.fa > ~/my1stproj/bucket/index/hum_index/hg38.cleaned.fa
+
+## check to make sure this worked
+awk '/^>chr/' hg38.cleaned.fa #should only get chr1-22, chrM, chrX, and chrY
+
+#chromosome sizes
+awk '!/GL|KI|alt/' ~/my1stproj/bucket/index/hum_index/hg38.chrom.sizes.txt > ~/my1stproj/bucket/index/hum_index/hg38.cleaned.chrom.sizes
+
+## transfer files to local after this 
+
+
+
+
+
+
+
+
+
+
 
 
 
